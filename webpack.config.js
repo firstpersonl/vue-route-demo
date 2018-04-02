f6ab6cdccd791d2fad353f48407d002561a7e57e
bysx <firstpersonl@163.com>
2018-04-02 18:32:34 +0800

    初步完成，开始对接后台接口

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './src/main.js',//值可以是字符串、数组或对象
     output: {
         path: path.resolve(__dirname, './dist'),//Webpack结果存储
-        publicPath: '/dist/',//懵懂，懵逼，//然而“publicPath”项则被许多Webpack的插件用于在生产模式和开发模式下下更新内嵌到css、html，img文件里的url值
+        publicPath: '/static/fonts/',//懵懂，懵逼，//然而“publicPath”项则被许多Webpack的插件用于在生产模式和开发模式下下更新内嵌到css、html，img文件里的url值
         filename: 'build.js'
     },
     module: {
